07872344c01311306e7448c64cb661db36f26b9e
Justin Vallelonga <jlvallelonga@gmail.com>
2021-04-14 12:37:31 -0600

    updates mac install with casks and xcode cmd line tools

--- mac_bootstrap.sh
+++ mac_bootstrap.sh
@@ -2,14 +2,27 @@
 
 echo 'Installing for Mac'
 
+xcode-select --install
+
 brew update
 
 # install oh my zsh first
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+brew install ripgrep
+brew install asdf
+
 # if using atom uncomment these:
 # brew cask install atom
 # apm install package-sync
 
-# always install ripgrep
-brew install ripgrep
+# caskable programs
+brew install --cask google-chrome
+brew install --cask iterm2
+brew install --cask hammerspoon
+brew install --cask spectacle
+brew install --cask postico
+brew install --cask docker
+brew install --cask zoom
+brew install --cask slack
+brew install --cask visual-studio-code
