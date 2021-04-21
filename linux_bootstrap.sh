3e11d0e8c2e9551cbae15fa8f1e12a742aab7add
Justin Vallelonga <jlvallelonga@gmail.com>
2021-04-20 22:44:21 -0600

    fixes linux bootstrap line continuation

--- linux_bootstrap.sh
+++ linux_bootstrap.sh
@@ -1,9 +1,23 @@
 echo '--- Installing for Linux ---'
 
 echo '--- Installing packages with apt-get ---'
-sudo apt update /
-&& apt -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev /
-libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk zsh
+sudo apt update  && apt -y install \
+build-essential \
+autoconf \
+m4 \
+libncurses5-dev \
+libwxgtk3.0-gtk3-dev \
+libgl1-mesa-dev \
+libglu1-mesa-dev \
+libpng-dev \
+libssh-dev \
+unixodbc-dev \
+xsltproc \
+fop \
+libxml2-utils \
+libncurses-dev \
+openjdk-11-jdk \
+zsh
 
 echo '--- Changing shell to zsh (relogin for this to take effect) ---'
 chsh -s $(which zsh)
