9b308ff8e692d63b46d4a6bb836b152c2bd67137
Justin Vallelonga <jlvallelonga@gmail.com>
2022-02-12 23:30:50 -0700

    updates pre_install script to not reinstall homebrew

--- pre_install.sh
+++ pre_install.sh
@@ -6,6 +6,11 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo '--- Installing xcode command line tools ---'
   xcode-select --install
 
+
   echo '--- Installing homebrew ---'
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if brew -v > /dev/null 2>&1; then
+    echo 'homebrew is already installed'
+  else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
 fi
